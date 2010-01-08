0d75c49976eb63e77d7754579f4cad07fa39bfac
Sarah Kreidler <sarah5280@gmail.com>
2010-01-08 23:23:11 +0000

    listener for change events on solve for (power or sample size) and showing curve

---
+++ src/edu/cudenver/bios/powercalculator/client/panels/OptionsListener.java
@@ -0,0 +1,8 @@
+package edu.cudenver.bios.powercalculator.client.panels;
+
+public interface OptionsListener
+{
+    public void onSolveFor(boolean solvingForPower);
+    
+    public void onShowCurve(boolean showCurve);
+}
