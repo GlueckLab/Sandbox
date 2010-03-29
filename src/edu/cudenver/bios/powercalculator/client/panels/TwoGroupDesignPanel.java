637e3e3b49f1cfaa7dc4232c6d9118568900e0f7
Sarah Kreidler <sarah5280@gmail.com>
2010-03-29 13:59:10 +0000

    added load from xml for upload functionality

--- src/edu/cudenver/bios/powercalculator/client/panels/TwoGroupDesignPanel.java
+++ src/edu/cudenver/bios/powercalculator/client/panels/TwoGroupDesignPanel.java
@@ -1,20 +1,25 @@
 package edu.cudenver.bios.powercalculator.client.panels;
 
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.xml.client.Document;
+import com.google.gwt.xml.client.NamedNodeMap;
+import com.google.gwt.xml.client.Node;
+import com.google.gwt.xml.client.NodeList;
 
 import edu.cudenver.bios.powercalculator.client.PowerCalculatorGUI;
 
 public class TwoGroupDesignPanel extends Composite
 {
 	private static final String STYLE = "twoGroupDesignPanel";
-    protected TextBox alpha = new TextBox();
-    protected TextBox mu0 = new TextBox();
-    protected TextBox muA = new TextBox();
-    protected TextBox sigma = new TextBox();
+    protected TextBox alphaTextBox = new TextBox();
+    protected TextBox mu0TextBox = new TextBox();
+    protected TextBox muATextBox = new TextBox();
+    protected TextBox sigmaTextBox = new TextBox();
     
     public TwoGroupDesignPanel()
     {
@@ -22,34 +27,93 @@ public class TwoGroupDesignPanel extends Composite
         VerticalPanel panel = new VerticalPanel();
         Grid grid = new Grid(4,2);
         grid.setWidget(0, 0, new HTML(PowerCalculatorGUI.constants.textLabelAlpha()));
-        grid.setWidget(0, 1, alpha);
+        grid.setWidget(0, 1, alphaTextBox);
         grid.setWidget(1, 0, new HTML(PowerCalculatorGUI.constants.textLabelMu0()));
-        grid.setWidget(1, 1, mu0);
+        grid.setWidget(1, 1, mu0TextBox);
         grid.setWidget(2, 0, new HTML(PowerCalculatorGUI.constants.textLabelMuA()));
-        grid.setWidget(2, 1, muA);
+        grid.setWidget(2, 1, muATextBox);
         grid.setWidget(3, 0, new HTML(PowerCalculatorGUI.constants.textLabelSigma()));
-        grid.setWidget(3, 1, sigma);
+        grid.setWidget(3, 1, sigmaTextBox);
         panel.add(grid);
         initWidget(panel);
     }
     
     public String getAlpha()
     {
-    	return alpha.getText();
+    	return alphaTextBox.getText();
     }
     
     public String getNullMean()
     {
-    	return mu0.getText();
+    	return mu0TextBox.getText();
     }
     
     public String getAlternativeMean()
     {
-    	return muA.getText();
+    	return muATextBox.getText();
     }
     
     public String getSigma()
     {
-    	return sigma.getText();
+    	return sigmaTextBox.getText();
     }        
+    
+    public void loadFromXMLDocument(Document doc)
+    {
+    	Window.alert("loading!");
+    	Node studyNode = doc.getElementsByTagName("study").item(0);
+    	if (studyNode != null)
+    	{
+    		Node alpha = studyNode.getAttributes().getNamedItem("alpha");
+    		if (alpha != null) alphaTextBox.setValue(alpha.getNodeValue());
+
+    		// parse the remaining matrices
+    		NodeList matrixNodes = doc.getElementsByTagName("matrix");
+    		for(int i = 0; i < matrixNodes.getLength(); i++)
+    		{
+    			Node matrixNode = matrixNodes.item(i);
+    			NamedNodeMap attrs = matrixNode.getAttributes();
+    			Node nameNode = attrs.getNamedItem("name");
+    			if (nameNode != null)
+    			{
+    				String name = nameNode.getNodeValue();
+    				if (name.equals("beta"))
+    				{
+    					// retrieve the mu0 (entry 0,0), muA (entry 1,0) from the beta matrix				
+    					NodeList rowNodeList = matrixNode.getChildNodes();
+    					for(int r = 0; r < rowNodeList.getLength(); r++)
+    					{
+    						Node firstColNode = rowNodeList.item(r).getFirstChild();
+    						if (firstColNode != null)
+    						{
+    							Node colItem = firstColNode.getFirstChild();
+    							if (r == 0)
+    							{
+    								if (colItem != null) mu0TextBox.setText(colItem.getNodeValue());
+    							}
+    							else if (r == 1)
+    							{
+    								if (colItem != null) muATextBox.setText(colItem.getNodeValue());
+    							}
+    						}
+    					}
+    				}
+    				else if (name.equals("sigmaError"))
+    				{
+    					// retrieve the variance from the sigma matrix (entry 0,0)		
+    					Node firstRowNode = matrixNode.getFirstChild();
+    					if (firstRowNode != null)
+    					{
+    						Node firstColNode = firstRowNode.getFirstChild();
+    						if (firstColNode != null)
+    						{
+    							Node colItem = firstColNode.getFirstChild();
+    							if (colItem != null) sigmaTextBox.setText(colItem.getNodeValue());
+    						}
+    					}
+    				}
+    			}
+    		}
+    	}
+    }
 }
