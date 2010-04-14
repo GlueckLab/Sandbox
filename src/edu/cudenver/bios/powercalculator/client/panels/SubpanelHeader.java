a56023655d4207fbf392072e79b3c3c3bdd5c01c
Sarah Kreidler <sarah5280@gmail.com>
2010-04-14 20:17:35 +0000

    added help link.  Still needs some layout/cleanup

--- src/edu/cudenver/bios/powercalculator/client/panels/SubpanelHeader.java
+++ src/edu/cudenver/bios/powercalculator/client/panels/SubpanelHeader.java
@@ -3,7 +3,9 @@ package edu.cudenver.bios.powercalculator.client.panels;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -14,14 +16,14 @@ public class SubpanelHeader extends Composite
     protected PopupPanel detailsPanel = null;
     protected HTML detailsHTML;
     protected HTML headerHTML;
+    protected HTML helpHTML = new HTML("?");
     
     public SubpanelHeader(String title, String details)
     {
-        VerticalPanel panel = new VerticalPanel();
+        HorizontalPanel panel = new HorizontalPanel();
         
         // create the header text
         headerHTML = new HTML(title);
-
         // create the popup text
         if (details != null && !details.isEmpty())
         {
@@ -35,17 +37,20 @@ public class SubpanelHeader extends Composite
             detailsPanel = new PopupPanel();
             detailsPanel.add(detailsHTML);
             
-            headerHTML.addClickHandler(new ClickHandler() {
+            helpHTML.addClickHandler(new ClickHandler() {
                 public void onClick(ClickEvent e)
                 {
-                    detailsPanel.showRelativeTo(headerHTML);
+                    detailsPanel.showRelativeTo(helpHTML);
                 }
             });
         }
         panel.add(headerHTML);
+        panel.add(helpHTML);
         
         headerHTML.setStyleName(PowerCalculatorConstants.STYLE_WIZARD_STEP_HEADER);
         headerHTML.addStyleDependentName(PowerCalculatorConstants.STYLE_WIZARD_STEP_SUBPANEL);
+        helpHTML.setStyleName("wizardStepHelp");
+        helpHTML.addStyleDependentName(PowerCalculatorConstants.STYLE_WIZARD_STEP_SUBPANEL);
         panel.setStyleName(PowerCalculatorConstants.STYLE_WIZARD_STEP_PANEL);
         panel.addStyleDependentName(PowerCalculatorConstants.STYLE_WIZARD_STEP_SUBPANEL);        
 
